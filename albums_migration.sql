891e83f2e463bc20a0e6b2add7970113147472ff
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-01-31 14:15:36 -0600

    Order by Exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,8 +1,8 @@
 USE codeup_test_db;
-    DROP TABLE IF EXISTS albums
+    DROP TABLE IF EXISTS albums;
         CREATE TABLE albums(
             id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-            ARTIST VARCHAR(50),
+            artist VARCHAR(50),
             name VARCHAR(50),
             release_date INT,
             sales float,
