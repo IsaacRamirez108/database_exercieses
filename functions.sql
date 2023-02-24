93be23f5540bf2599d9f6ce27405aba0239d5417
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-02-23 22:59:08 -0600

    Functions Exercise Updates

--- functions.sql
+++ functions.sql
@@ -9,3 +9,10 @@ FROM employees
 WHERE YEAR(hire_date) BETWEEN 1990 AND 1999
   AND MONTH(birth_date) = 12
   AND DAYOFMONTH(birth_date) = 25;
+
+SELECT *
+FROM employees
+WHERE YEAR(hire_date) BETWEEN 1990 AND 1999
+  AND MONTH(birth_date) = 12
+  AND DAYOFMONTH(birth_date) = 25
+ORDER BY birth_date, hire_date DESC;
